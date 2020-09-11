ab6257531afc265b0b3d3c66bb893e4294f36e6d
Dr479 <70656629+Dr479@users.noreply.github.com>
2020-09-11 14:33:32 -0400

    Add files via upload

---
+++ Homework.java
@@ -0,0 +1,17 @@
+public class Homework
+{
+
+   public static void main(String[] args)
+   {
+   
+      String varName = "john";
+      System.out.println(varName);
+      
+      int varAge = 22;
+      System.out.println(varAge);
+      
+      float varDec = 2.5f;
+      System.out.println(varDec);
+    }
+      
+}
