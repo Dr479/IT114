a0e7d7e376ce97d59c04bfa73c9a0b8e6d6ad695
Dr479 <70656629+Dr479@users.noreply.github.com>
2020-09-20 16:13:05 -0400

    Add files via upload

---
+++ JavaLoops.java
@@ -0,0 +1,22 @@
+public class JavaLoops
+{
+   public static void main(String[] args) 
+   {
+	   int[] numbers = {4,5,6,7,8,9,10};
+      
+      for (int i = 0; i < numbers.length; i++)
+        System.out.println(numbers[i]);
+        
+      for (int i = 0; i < numbers.length; i++)
+         if (i % 2 == 0) 
+            System.out.print(numbers[i]+ " ");
+            
+      
+      
+
+         
+           
+
+         
+   }
+}
